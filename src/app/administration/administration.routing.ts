5c463ea8a9848646ee9a6faa980c970ebd51bf91
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2021-03-12 19:42:22 -0400

    [UPDATE]

--- src/app/administration/administration.routing.ts
+++ src/app/administration/administration.routing.ts
@@ -1,9 +1,29 @@
 import { Routes } from '@angular/router';
 import { ContentManagementComponent } from './content-management/content-management.component';
+import { BlocManagementComponent } from './content-management/bloc-management/bloc-management.component';
+import { InfoManagementComponent } from './content-management/info-management/info-management.component';
+import { TeamManagementComponent } from './content-management/team-management/team-management.component';
+import { SponsorsManagementComponent } from './content-management/sponsors-management/sponsors-management.component';
 
 export const AdministrationRoutingModule: Routes = [
   {
     path: "content",
     component: ContentManagementComponent
+  },
+  {
+    path: "bloc",
+    component: BlocManagementComponent
+  },
+  {
+    path: "info",
+    component: InfoManagementComponent
+  },
+  {
+    path: "team",
+    component: TeamManagementComponent
+  },
+  {
+    path: "sponsors",
+    component: SponsorsManagementComponent
   }
 ];
